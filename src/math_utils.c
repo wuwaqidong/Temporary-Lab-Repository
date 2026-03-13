d0a25c29b095fe402c50293e04cdef60080e85d2
wuwaqidong <1210787512@qq.com>
2026-03-13 20:44:11 +0800

    build math_utils.c

---
+++ src/math_utils.c
@@ -0,0 +1,10 @@
+#include "math_utils.h"
+
+int factorial(int n) {
+    if (n < 0) return -1;
+    int result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
